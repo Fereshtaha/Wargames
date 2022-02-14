02d58113234c193f67d256b0bc991140bf000bb5
Fereshta <fereshta@live.no>
2022-02-14 16:27:43 +0100

    lagt til CommanderUnit

--- src/main/java/CommanderUnit.java
+++ src/main/java/CommanderUnit.java
@@ -1,2 +1,13 @@
-public class CommanderUnit {
+public class CommanderUnit extends CavalryUnit {
+
+    public CommanderUnit(String name, int health, int armor, int attack) {
+        super(name,health,attack,armor);
+    }
+
+    public CommanderUnit(String name, int health) {
+        super(name, health);
+        this.attack = 25;
+        this.armor = 15;
+    }
+
 }
